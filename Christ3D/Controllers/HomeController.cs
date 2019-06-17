93454c2ce58b334626d8cbf4193b604c9cd1fe7a
T_T <46785596+IanROX@users.noreply.github.com>
2019-06-17 19:18:11 +0800

    submit test1
    submit test1

--- Christ3D/Controllers/HomeController.cs
+++ Christ3D/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace Christ3D.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page.11";
 
             return View();
         }
